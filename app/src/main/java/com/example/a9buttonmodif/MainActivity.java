26fe89dbf0ada1efd57fdbb354bf0272a01ccf3c
brtrando <115710114+brtrando@users.noreply.github.com>
2022-11-24 10:09:04 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/a9buttonmodif/MainActivity.java
+++ app/src/main/java/com/example/a9buttonmodif/MainActivity.java
@@ -28,15 +28,25 @@ public class MainActivity extends AppCompatActivity {
         b7=(Button)findViewById(R.id.button7);
         b8=(Button)findViewById(R.id.button8);
         b9=(Button)findViewById(R.id.button9);
-        b1.setText(nama[0]);
-        b2.setText(nama[1]);
-        b3.setText(nama[2]);
-        b4.setText(nama[3]);
-        b5.setText(nama[4]);
-        b6.setText(nama[5]);
-        b7.setText(nama[6]);
-        b8.setText(nama[7]);
-        b9.setText(nama[8]);
+        Random r = new Random();
+                i = r.nextInt(16-1)+1;
+                i2 = r.nextInt(16-1)+1;
+                i3 = r.nextInt(16-1)+1;
+                i4 = r.nextInt(16-1)+1;
+                i5 = r.nextInt(16-1)+1;
+                i6 = r.nextInt(16-1)+1;
+                i7 = r.nextInt(16-1)+1;
+                i8 = r.nextInt(16-1)+1;
+                i9 = r.nextInt(16-1)+1;
+                b1.setText(String.valueOf(nama[i]));
+                b2.setText(String.valueOf(nama[i2]));
+                b3.setText(String.valueOf(nama[i3]));
+                b4.setText(String.valueOf(nama[i4]));
+                b5.setText(String.valueOf(nama[i5]));
+                b6.setText(String.valueOf(nama[i6]));
+                b7.setText(String.valueOf(nama[i7]));
+                b8.setText(String.valueOf(nama[i8]));
+                b9.setText(String.valueOf(nama[i9]));
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
